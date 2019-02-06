1139323444a149cfcf97f2ec2075be1b447c379b
karanpannu <karanbir.pannu.92@gmail.com>
2019-02-05 20:27:03 -0500

    Started working on GamePlay Model

--- src/com/risk/model/GamePlay.java
+++ src/com/risk/model/GamePlay.java
@@ -1,6 +1,38 @@
 package com.risk.model;
 
-public class GamePlay {
+
+import java.util.ArrayList;
+import java.util.Observable;
+/**
+ * "GamePlay" model basically consists of an object that consist of the current map that the game is being played upon
+ * and the players that are playing the game.
+ */
+
+public class GamePlay extends Observable
+{
+    /** MapRiskmodel
+     */
+    private MapRisk mapRiskObj = new MapRisk();
+
+    /** current players playing the game*/
+    private ArrayList<Player> currentPlayers = new ArrayList<Player>();
+
+    /** All the cards*/
+    private ArrayList<Card> deckOfCards = new ArrayList<Card>();
+
+    /** if country owned*/
+    private boolean ifCountryOwned = false;
+
+    /**
+     * Default Constructor.
+     */
+    public GamePlay(MapRisk mapRiskObj, ArrayList<Player> currentPlayers, ArrayList<Card> deckOfCards, boolean ifCountryOwned)
+    {
+        this.mapRiskObj = mapRiskObj;
+        this.currentPlayers = currentPlayers;
+        this.deckOfCards = deckOfCards;
+        this.ifCountryOwned = ifCountryOwned;
+    }
 
 
 }
