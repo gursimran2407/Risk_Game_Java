ca2d49fe6fdcc84ea7a18f0921c3f5d9de9844a8
gursimransingh <gursimran81@gmail.com>
2019-02-06 13:13:32 -0500

    Added JavaFX support and made intial UI for the app.

---
+++ src/com/risk/controller/mainGame.java
@@ -0,0 +1,38 @@
+package com.risk.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class mainGame extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("com/risk/controller/mainGame.fxml"));
+        primaryStage.setTitle("PLAY RISK!!");
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() throws Exception {
+        System.out.printf("INIT method called");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("STop METHOD!");
+    }
+}
--- src/com/risk/view/main.java
+++
@@ -1,7 +0,0 @@
-package com.risk.view;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.printf("HELLO");
-    }
-}
