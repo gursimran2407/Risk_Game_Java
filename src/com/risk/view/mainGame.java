3de08b81f12b0877832f8afffea0ad8793857eed
Karandeep <Karandiip@gmail.com>
2019-02-08 13:08:11 -0500

    Edited message Line 37

--- src/com/risk/view/mainGame.java
+++ src/com/risk/view/mainGame.java
@@ -34,7 +34,7 @@ public class mainGame extends Application {
 
     @Override
     public void stop() throws Exception {
-        System.out.println("\nStop METHOD!");
+        System.out.println("\n Stop METHOD!");
     }
 }
 
