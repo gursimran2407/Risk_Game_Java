90abde497859598db2812ada02b22eb21b423fd8
gursimransingh <gursimran81@gmail.com>
2019-02-09 14:07:41 -0500

    Created mechanism to add countries as well.

---
+++ src/com/risk/view/AddCountryDialogController.java
@@ -0,0 +1,12 @@
+package com.risk.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * @author gursimransingh
+ */
+public class AddCountryDialogController {
+    @FXML
+    private TextField countryNameTextField;
+}
--- src/com/risk/view/MapEditorController.java
+++ src/com/risk/view/MapEditorController.java
@@ -1,7 +1,6 @@
 package com.risk.view;
 
 
-import com.risk.model.CountryModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,22 +12,30 @@ import java.util.Optional;
 
 public class MapEditorController {
     @FXML
+    private Button addCountryButton;
+    @FXML
+    private ListView countryListView;
+    @FXML
     private ListView continentListView;
     @FXML
     private BorderPane mapEditorBorderPane;
     @FXML
-    private Button addButton;
+    private Button addContinentButton;
 
 
     public void initialize() {
-        //Updating listview when app is initialized
+        //Updating continentlistview when app is initialized
         continentListView.setItems(ContinentData.getInstance().getContinents());
+        continentListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        continentListView.getSelectionModel().selectFirst();
+
+        //
 
     }
 
 
     public void handleAddDialog(ActionEvent event) {
-        if (event.getSource() == addButton) {
+        if (event.getSource() == addContinentButton) {
             //Creating a dialog to enter continents
             Dialog<ButtonType> dialog = new Dialog<>();
             dialog.initOwner(mapEditorBorderPane.getScene().getWindow());
@@ -36,6 +43,32 @@ public class MapEditorController {
             dialog.setHeaderText("Add continents to the game along with their control values");
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource("addContinentDIalog.fxml"));
+
+            try {
+                dialog.getDialogPane().setContent(fxmlLoader.load());
+            } catch (IOException e) {
+                System.out.println("Can't load addcontinent dialog!");
+                e.printStackTrace();
+                return;
+            }
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+            Optional<ButtonType> result = dialog.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                System.out.println("OK pressed");
+                AddContinentDIalogController addContinentDIalogController = fxmlLoader.getController();
+                addContinentDIalogController.addNewContinent();
+
+            }
+        } else if (event.getSource() == addCountryButton) {
+            //Creating a dialog to enter continents
+            Dialog<ButtonType> dialog = new Dialog<>();
+            dialog.initOwner(mapEditorBorderPane.getScene().getWindow());
+            dialog.setTitle("Add Continents!");
+            dialog.setHeaderText("Add continents to the game along with their control values");
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("addCountryDialog.fxml.fxml"));
 
             try {
                 dialog.getDialogPane().setContent(fxmlLoader.load());
