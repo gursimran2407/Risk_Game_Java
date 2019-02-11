86155a03da52e25b006f1552528cf238251aebfa
Karandeep <Karandiip@gmail.com>
2019-02-11 00:51:43 -0500

    Created strategy interface

---
+++ src/com/risk/helperInterfaces/StrategyInterface.java
@@ -0,0 +1,15 @@
+package com.risk.helperInterfaces;
+
+/**
+ * This is the main interface of all three phases
+ * @author Karandeep
+ */
+
+public interface StrategyInterface {
+
+    public void fortificationPhase();
+
+    public void reinforcementPhase();
+
+    public void attackPhase();
+}
