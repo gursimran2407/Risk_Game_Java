5e03edbd9e5f2fd2b2b9f38141bf1f88588dbda2
Namita Faujdar <namita.mits26@gmail.com>
2019-02-11 00:42:48 -0500

    Added method to update the window

--- src/com/risk/view/Reinforcement.java
+++ src/com/risk/view/Reinforcement.java
@@ -8,13 +8,23 @@ import com.risk.model.GamePlayModel;
 /** this is the view that is going to be used for the reinforcement phase of the game.
  * @author Karan
  */
-public class Reinforcement
-{
+public class Reinforcement {
 
     private MapRiskModel mapRiskModelModOjb;
 
     private PlayerModel playerModelObj;
 
     private GamePlayModel gamePlayObj;
+
+    /**
+     * This updateWindow method is called whenever the model is updated. It updates
+     * the Screen for Reinforcement Phase
+     *
+     * @param gamePlayModel
+     * @param playerModel
+     */
+    public void updateWindow(GamePlayModel gamePlayModel, PlayerModel playerModel) {
+
+    }
 
 }
