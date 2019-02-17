85929e240cc803520481d8a76ab27071126f9b25
gursimransingh <gursimran81@gmail.com>
2019-02-17 16:52:11 -0500

    Courage to delete the code!

--- src/com/risk/controller/MapCreateContinentController.java
+++ src/com/risk/controller/MapCreateContinentController.java
@@ -1,7 +1,19 @@
 package com.risk.controller;
 
+import com.risk.model.MapRiskModel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
+ * Map continent Controller
  * @author gursimransingh
  */
-public class MapCreateContinentController {
+public class MapCreateContinentController implements ActionListener {
+    MapRiskModel mapRiskModel;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
