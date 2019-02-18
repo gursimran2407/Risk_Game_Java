9ed00173c4e78d045781b42e75b23dd02f688503
Karandeep <Karandiip@gmail.com>
2019-02-18 14:05:20 -0500

    Update view lists

--- src/com/risk/controller/MapCreateContinentController.java
+++ src/com/risk/controller/MapCreateContinentController.java
@@ -26,7 +26,7 @@ public class MapCreateContinentController implements ActionListener {
     public MapCreateContinentController() {
         mapRiskModel = new MapRiskModel();
         mapCreateContinentView = new MapCreateContinentView();
-        //Initializing Arraylist to store continent models
+        //Initializing ArraylList to store continent models
 
         mapRiskModel.addObserver(mapCreateContinentView);
         mapCreateContinentView.setActionListener(this);
@@ -90,8 +90,8 @@ public class MapCreateContinentController implements ActionListener {
                         JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{}, null);
                 return;
             } else {
-                //Creating Arraylist of Arraylist of points
-                //Creating ColorList of Colorlist
+                //Creating ArrayList of ArrayList of points
+                //Creating ColorList of ColorList
                 ArrayList<ArrayList<Point>> pointsList = new ArrayList<>();
                 ArrayList<Color> colorList = new ArrayList<>();
 
@@ -155,10 +155,5 @@ public class MapCreateContinentController implements ActionListener {
                 this.mapCreateContinentView.dispose();
             }
         }
-
-
-
     }
-
-
 }
