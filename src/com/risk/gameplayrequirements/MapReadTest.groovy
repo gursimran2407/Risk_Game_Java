2bbcdf93e436c1d69b77a2d37de8fe7093f1a4ce
Shriyans Athalye <shriyansathalye@gmail.com>
2019-02-23 17:44:45 -0500

    Created Test cases for validating map file

---
+++ src/com/risk/gameplayrequirements/MapReadTest.groovy
@@ -0,0 +1,15 @@
+package com.risk.gameplayrequirements
+
+class MapReadTest extends GroovyTestCase {
+    void testGetMapContinentDetails() {
+    }
+
+    void testGetMapCountryDetails() {
+    }
+
+    void testGetReadFile() {
+    }
+
+    void testSetReadFile() {
+    }
+}
