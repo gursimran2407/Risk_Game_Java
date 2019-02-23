61ee8f51d69f31ddfb4493bdd8e6269aeb3ef7cf
Karandeep <Karandiip@gmail.com>
2019-02-23 15:13:35 -0500

    Created fortification controller

---
+++ src/com/risk/controller/FortificationController.java
@@ -0,0 +1,10 @@
+package com.risk.controller;
+
+/**
+ * In FortificationController, the data flow into model object and updates the
+ * view whenever data changes.
+ * @author Karandeep
+ */
+
+public class FortificationController {
+}
