d13207aeb33a5656217350796be668170cc1b030
gursimransingh <gursimran81@gmail.com>
2019-02-26 13:41:47 -0500

    Created Test folder. Add all test cases inside the test folder: /src/test/

---
+++ test/com/risk/model/MapReadTest.java
@@ -0,0 +1,22 @@
+//package com.risk.model;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class MapReadTest {
+//
+//    @org.junit.jupiter.api.Test
+//    void getMapContinentDetails() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void getMapCountryDetails() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void getReadFile() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void setReadFile() {
+//    }
+//}
