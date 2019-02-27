e59472adb761b646eb844240c476cbf8ca08be93
Karandeep <Karandiip@gmail.com>
2019-02-27 09:16:09 -0500

    Java doc in view interface

--- src/com/risk/helperInterfaces/ViewInterface.java
+++ src/com/risk/helperInterfaces/ViewInterface.java
@@ -4,9 +4,15 @@ import java.awt.event.ActionListener;
 import java.util.Observer;
 
 /**
+ * interface to build view class
+ *
  * @author gursimransingh
  */
 public interface ViewInterface extends Observer {
-
+    /**
+     * Sets Action Listener to listen to the event
+     *
+     * @param actionListener
+     */
     void setActionListener(ActionListener actionListener);
 }
