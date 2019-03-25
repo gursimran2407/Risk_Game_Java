672effb24074cdaf74613d72994ef552924e9c4b
karanpannu <karanbir.pannu.92@gmail.com>
2019-03-24 20:39:15 -0400

    In the last Demo we lost a mark because of not printing some of the messages on the console. I am referring to the output messages as in the number of armies, the name of the player, the countries etc. implementing the classes - Constant, MessageUtil and MapButton classes for the same. Constant class is loading the map file and the MapButton is for the positioning of the country.

---
+++ src/com/risk/utilities/Constant.java
@@ -0,0 +1,13 @@
+package com.risk.utilities;
+
+/**
+ * Constant file to load the map fie
+ *
+ * @author KaranPannu
+ *
+ */
+
+public class Constant {
+    //public static final String FILE_LOCATION = "C:\\Suruthi\\adv 6441\\others\\Soen-6441-Online-risk\\OnlineRiskGame\\res\\World.map";
+    public static final String FILE_LOCATION = "OnlineRiskGame\\res\\World.map";
+}
