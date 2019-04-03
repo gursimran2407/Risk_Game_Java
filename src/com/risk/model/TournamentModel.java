f126985a962dba32d1124a5541870e0825eb783e
gursimransingh <gursimran81@gmail.com>
2019-04-03 09:48:52 -0400

    Created TournmentModel.java and extended the Observable class. This class is used to store data related to tournment mode in the game

--- src/com/risk/model/TournamentModel.java
+++ src/com/risk/model/TournamentModel.java
@@ -1,7 +1,64 @@
 package com.risk.model;
 
+import java.util.ArrayList;
+import java.util.Observable;
+
 /**
  * @author gursimransingh
  */
-public class TournamentModel {
+public class TournamentModel extends Observable {
+    /**
+     * The game Play model.
+     */
+    private ArrayList<GamePlayModel> gamePlayModels = new ArrayList<GamePlayModel>();
+
+    /**
+     * The no of games.
+     */
+    private int noOfGames;
+
+    /**
+     * The no of turns.
+     */
+    private int noOfTurns;
+
+    public TournamentModel(ArrayList<GamePlayModel> gamePlayModels, int noOfGames, int noOfTurns) {
+        this.gamePlayModels = gamePlayModels;
+        this.noOfGames = noOfGames;
+        this.noOfTurns = noOfTurns;
+    }
+
+
+    public TournamentModel() {
+    }
+
+    public ArrayList<GamePlayModel> getGamePlayModels() {
+        return gamePlayModels;
+    }
+
+    public void setGamePlayModels(ArrayList<GamePlayModel> gamePlayModels) {
+        this.gamePlayModels = gamePlayModels;
+    }
+
+    public int getNoOfGames() {
+        return noOfGames;
+    }
+
+    public void setNoOfGames(int noOfGames) {
+        this.noOfGames = noOfGames;
+    }
+
+    public int getNoOfTurns() {
+        return noOfTurns;
+    }
+
+    public void setNoOfTurns(int noOfTurns) {
+        this.noOfTurns = noOfTurns;
+    }
+
+    public void callObservers() {
+        setChanged();
+        notifyObservers(this);
+    }
+
 }
