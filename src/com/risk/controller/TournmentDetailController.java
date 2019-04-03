b2629a4bf0fa4a165336a906afe1b17936c99e94
gursimransingh <gursimran81@gmail.com>
2019-04-03 09:55:19 -0400

    Worked on tournment Detail Controller class to create methods related to the working of the tournment mode in the game

--- src/com/risk/controller/TournmentDetailController.java
+++ src/com/risk/controller/TournmentDetailController.java
@@ -4,7 +4,9 @@ import com.risk.model.GamePlayModel;
 import com.risk.model.PlayerModel;
 import com.risk.model.TournamentModel;
 import com.risk.view.TournamentDetailView;
+import org.json.simple.parser.ParseException;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -84,7 +86,73 @@ public class TournmentDetailController implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource().equals(theTournamentDetailView.saveAndPlayButton)) {
+            validGame = true;
+            noOfGames = (int) theTournamentDetailView.noOfGames.getSelectedItem();
+            this.theTournamentModel.setNoOfGames(noOfGames);
+            noOfPlayers = (int) theTournamentDetailView.noOfPlayers.getSelectedItem();
+            try {
+                playerValidation();
+            } catch (ParseException e) {
+                validGame = false;
+                e.printStackTrace();
+            }
+            noOfTurns = (int) theTournamentDetailView.noOfTurnsText.getSelectedItem();
+            if (mapLoaded != true) {
+                validGame = false;
+            }
+            if (validGame == true) {
+                for (int i = 0; i < noOfGames; i++) {
+                    for (int j = 0; j < this.theTournamentModel.getGamePlay().size(); j++) {
+                        new StartUpTournamentController(this.theTournamentModel.getGamePlay().get(j), noOfTurns);
+                    }
+                }
+                this.theTournamentDetailView.dispose();
+            }
+        } else if (actionEvent.getSource().equals(theTournamentDetailView.exitButton)) {
+            this.theTournamentDetailView.dispose();
+        } else if (actionEvent.getSource().equals(theTournamentDetailView.browseMap1Button)) {
+            int value = theTournamentDetailView.chooseMap1.showOpenDialog(theTournamentDetailView);
+            if (value == JFileChooser.APPROVE_OPTION) {
+                mapFile[0] = theTournamentDetailView.chooseMap1.getSelectedFile();
+            }
+        } else if (actionEvent.getSource().equals(theTournamentDetailView.browseMap2Button)) {
+            int value = theTournamentDetailView.chooseMap2.showOpenDialog(theTournamentDetailView);
+            if (value == JFileChooser.APPROVE_OPTION) {
+                mapFile[1] = theTournamentDetailView.chooseMap2.getSelectedFile();
+            }
+        } else if (actionEvent.getSource().equals(theTournamentDetailView.browseMap3Button)) {
+            int value = theTournamentDetailView.chooseMap3.showOpenDialog(theTournamentDetailView);
+            if (value == JFileChooser.APPROVE_OPTION) {
+                mapFile[2] = theTournamentDetailView.chooseMap3.getSelectedFile();
+            }
+        } else if (actionEvent.getSource().equals(theTournamentDetailView.browseMap4Button)) {
+            int value = theTournamentDetailView.chooseMap4.showOpenDialog(theTournamentDetailView);
+            if (value == JFileChooser.APPROVE_OPTION) {
+                mapFile[3] = theTournamentDetailView.chooseMap4.getSelectedFile();
+            }
+        } else if (actionEvent.getSource().equals(theTournamentDetailView.browseMap5Button)) {
+            int value = theTournamentDetailView.chooseMap5.showOpenDialog(theTournamentDetailView);
+            if (value == JFileChooser.APPROVE_OPTION) {
+                mapFile[4] = theTournamentDetailView.chooseMap5.getSelectedFile();
+            }
+        } else if (actionEvent.getSource().equals(theTournamentDetailView.validateMapButton)) {
+
+            noOfMaps = (int) theTournamentDetailView.noOfMaps.getSelectedItem();
+            for (int i = 0; i < noOfMaps; i++) {
+                if (mapFile[i] == null) {
+                    JOptionPane.showOptionDialog(null, "Select the " + (i + 1) + " appropriate maps", "Invalid",
+                            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{}, null);
+                } else {
+                    if (mapVerification(mapFile[i], i)) {
+                        if (noOfMaps == (i + 1)) {
+                            mapLoaded = true;
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
