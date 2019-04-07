3f1c7d6e2669d34162132833bee84dcd3efc59cd
Shriyans Athalye <shriyansathalye@gmail.com>
2019-04-06 21:43:39 -0400

    Added attack method in cheaterplayer controller

--- src/com/risk/controller/CheaterPlayerController.java
+++ src/com/risk/controller/CheaterPlayerController.java
@@ -1,6 +1,7 @@
 package com.risk.controller;
 
 import com.risk.helperInterfaces.Strategy;
+import com.risk.model.CountryModel;
 import com.risk.model.GamePlayModel;
 import com.risk.utilities.Validation;
 
@@ -30,6 +31,9 @@ public class CheaterPlayerController implements Strategy {
         this.gamePlayModel = gamePlayModel;
     }
 
+    /**
+     * This method will be called during fortification phase
+     */
     @Override
     public void fortification() {
         this.gamePlayModel.getConsole().append("Cheater - fortification");
@@ -57,7 +61,50 @@ public class CheaterPlayerController implements Strategy {
 
     @Override
     public void attack() {
+        this.gamePlayModel.getConsole().append("Cheater - attack");
+        index1 = getRandomBetweenRange(1, this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().size());
+        index1 = index1 - 1;
 
+        CountryModel attackCountry = this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(index1);
+        this.gamePlayModel.getConsole().append("Attack Country - " + attackCountry.getCountryName());
+        for (int i = 0; i < attackCountry.getLinkedCountries().size(); i++) {
+            for (int j = 0; j < this.gamePlayModel.getGameMap().getCountries().size(); j++) {
+                if (attackCountry.getLinkedCountries().get(i).getCountryName()
+                        .equals(this.gamePlayModel.getGameMap().getCountries().get(j).getCountryName())) {
+                    if (this.gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer()
+                            .equals(this.gamePlayModel.getGameMap().getCountries().get(j).getRulerName())) {
+                        continue;
+                    } else {
+                        for (int k = 0; k < this.gamePlayModel.getPlayers().size(); k++) {
+                            if (this.gamePlayModel.getGameMap().getCountries().get(j).getRulerName()
+                                    .equals(this.gamePlayModel.getPlayers().get(k).getNamePlayer())) {
+                                this.gamePlayModel.getPlayers().get(k)
+                                        .defend(this.gamePlayModel.getGameMap().getCountries().get(j));
+                                this.gamePlayModel.getConsole()
+                                        .append(" Country "
+                                                + this.gamePlayModel.getGameMap().getCountries().get(j).getCountryName()
+                                                + "is removed from player "
+                                                + this.gamePlayModel.getPlayers().get(k).getNamePlayer());
+                            }
+                            if (this.gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer()
+                                    .equals(this.gamePlayModel.getPlayers().get(k).getNamePlayer())) {
+                                this.gamePlayModel.getPlayers().get(k)
+                                        .attacked(this.gamePlayModel.getGameMap().getCountries().get(j));
+                                this.gamePlayModel.getConsole()
+                                        .append(" Country "
+                                                + this.gamePlayModel.getGameMap().getCountries().get(j).getCountryName()
+                                                + "is added to player "
+                                                + this.gamePlayModel.getPlayers().get(k).getNamePlayer());
+                            }
+                        }
+                        this.gamePlayModel.getGameMap().getPlayerTurn()
+                                .attacked(this.gamePlayModel.getGameMap().getCountries().get(j));
+                        this.gamePlayModel.getGameMap().getCountries().get(j)
+                                .setRulerName(this.gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer());
+                    }
+                }
+            }
+        }
     }
 
     @Override
