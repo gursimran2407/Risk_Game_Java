3aac1b309355a43fa2bd1099c04c7303a061e887
gursimransingh <gursimran81@gmail.com>
2019-04-08 12:02:53 -0400

    Working on ValidateMapButton in TournmentDetailController.java

--- src/com/risk/controller/TournmentDetailController.java
+++ src/com/risk/controller/TournmentDetailController.java
@@ -127,19 +127,7 @@ public class TournmentDetailController implements ActionListener {
             }
         } else if (actionEvent.getSource().equals(theTournamentDetailView.validateMapButton)) {
 
-            noOfMaps = (int) theTournamentDetailView.noOfMaps.getSelectedItem();
-            for (int i = 0; i < noOfMaps; i++) {
-                if (mapFile[i] == null) {
-                    JOptionPane.showOptionDialog(null, "Select the " + (i + 1) + " appropriate maps", "Invalid",
-                            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[] {}, null);
-                } else {
-                    if (mapVerification(mapFile[i], i)) {
-                        if (noOfMaps == (i + 1)) {
-                            mapLoaded = true;
-                        }
-                    }
-                }
-            }
+            //To be rectified
         }
     }
 
